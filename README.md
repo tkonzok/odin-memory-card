09b7893a47c0ff2d2a4df38b8166c0359125aca3
Tobias Konzok <konzok.tobias@gmail.com>
2023-08-07 18:01:14 +0200

    Init working game

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Image by <a href="https://pixabay.com/users/felixmittermeier-4397258/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2675322">PayPal.me/FelixMittermeier</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2675322">Pixabay</a>
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function Highscore({ highscore }) {
+  return <p className="highscore">Highscore: {highscore}</p>;
+}
+
+function Score({ score }) {
+  return <p className="score">Your score: {score}</p>;
+}
+
+function Card({ character, onClick }) {
+  function handleClick() {
+    return onClick(character.id);
+  }
+
+  return (
+    <button key={character.id} className="card" onClick={handleClick}>
+      <img src={character.image} width="250px"></img>
+      <div>{character.name}</div>
+    </button>
+  );
+}
+
+function Gameboard({ characters, onClick }) {
+  let cards = [];
+  for (const character of characters) {
+    cards.push(
+      <Card key={character.id} character={character} onClick={onClick} />
+    );
+  }
+
+  return <div className="gameboard">{cards}</div>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "https://rickandmortyapi.com/api/character/";
+
+  const [isBusy, setIsBusy] = useState(true);
+  const [highscore, setHighscore] = useState(0);
+  const [score, setScore] = useState(0);
+  const [data, setData] = useState([]);
+  const [available, setAvailable] = useState([]);
+  const [guessed, setGuessed] = useState([]);
+  const [characters, setCharacters] = useState([]);
+
+  function getRandomDataset(count = 32) {
+    let ids = [];
+    let guardian = new Set();
+    while (ids.length < count) {
+      const randomId = Math.floor(Math.random() * 826);
+      if (guardian.has(randomId)) {
+        continue;
+      }
+      guardian.add(randomId);
+      ids.push(randomId);
+    }
+    ids.sort((a, b) => a - b);
+    return ids;
+  }
+
+  function fetchCards() {
+    let characterInfos = [];
+    let guardian = new Set();
+    while (characterInfos.length < 8) {
+      const randomId = Math.floor(Math.random() * data.length);
+      if (guardian.has(randomId)) {
+        continue;
+      }
+      if (guessed.includes(randomId)) {
+        continue;
+      }
+      const element = data[randomId];
+      guardian.add(randomId);
+      characterInfos.push(element);
+    }
+    return characterInfos;
+  }
+
+  function getData(receivedData) {
+    if (!guessed.includes(receivedData)) {
+      setGuessed([...guessed, receivedData]);
+      setAvailable(
+        available.filter((character) => character.id !== receivedData)
+      );
+      setScore(score + 1);
+      nextRound();
+    } else {
+      setHighscore(score);
+      console.log("Game Over!");
+    }
+  }
+
+  function nextRound() {
+    const newCards = fetchCards();
+    setCharacters(newCards);
+  }
+
+  useEffect(() => {
+    const dataset = getRandomDataset();
+    const fetchData = async () => {
+      const response = await fetch(url + `${dataset}`);
+      const apiData = await response.json();
+      setData(apiData);
+    };
+    fetchData();
+  }, []);
+
+  if (data.length > 0 && characters.length < 1) {
+    setCharacters(fetchCards());
+    setIsBusy(false);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <Highscore highscore={highscore} />
+        <h1>Rick and Morty</h1>
+        <h2>Memory Card Game</h2>
+        <Score score={score} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isBusy && <Gameboard characters={characters} onClick={getData} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
